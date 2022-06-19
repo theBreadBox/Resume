e2aefe3d6cd68f43321036f634baed2dfa67a769
theBreadBox <97469184+theBreadBox@users.noreply.github.com>
2022-06-19 10:23:25 -0600

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,137 @@
+const logotext = "Brendan O'Flaherty";
+const meta = {
+    title: "Brendan Resume",
+    description: "I’m Brendan, and I am a  _ blockchain devloper,currently living in Colorado",
+};
+
+const introdata = {
+    title: "I’m Brendan, and",
+    animated: {
+        first: "I love Web3",
+        second: "I write smart contracts",
+        third: "I develop NFTs",
+    },
+    description: "Blockchain is the future. Be ready.",
+    your_img_url: "https://static.wixstatic.com/media/d0fb19_485abdbf358041618423ee213c842bb8~mv2.gif",
+};
+
+const dataabout = {
+    title: "Background",
+    aboutme: "Originally from upstate New York, my wife and I moved to Colorado in 2012. I entered the blockchain space in 2016, coming from a cyber security program. I started writing smart contracts in 2017 while working on a proprietary chain called PhilosBDL. Shortly after that I entered the NFT space, and have since launced numerous NFT projects to the Ethereum Mainnet generating over $500k in sales. ",
+};
+const worktimeline = [
+    {
+        jobtitle: "PepsiCo",
+        where: "TBA",
+        date: "2022",
+    },
+    {
+        jobtitle: "LoverBoys",
+        where: "ETH",
+        date: "06/2022",
+    },
+    {
+        jobtitle: "Baked Blue Jays",
+        where: "ETH",
+        date: "05/2022",
+    },
+    {
+        jobtitle: "Alivacious",
+        where: "ETH",
+        date: "02/2022",
+    },
+    {
+        jobtitle: "Bud Buddies",
+        where: "ETH",
+        date: "11/2021",
+    },
+];
+
+const skills = [{
+        name: "Solidity",
+        value: 95,
+    },
+    {
+        name: "React",
+        value: 90,
+    },
+    {
+        name: "Javascript",
+        value: 95,
+    },
+    {
+        name: "POC Development",
+        value: 100,
+    },
+    {
+        name: "Team Building",
+        value: 100,
+    },
+];
+
+const services = [{
+        title: "NFTs",
+        description: "Helping teams and creators from start to finish launch a successful NFT project. This includes, art generation assistance, smart contract authoring, minting website creation, and deployment walkthroughs.",
+    },
+    {
+        title: "Web3",
+        description: "Web3 enabled website buildout from the ground up, or as an addition to an exisiting website. This includes, NFT minting, token based authorizations, and DeFi.",
+    },
+    {
+        title: "Solutions Architect",
+        description: "Even if you are in the infant stages of development, or you have a problem you think can be solved utilizing blockchain technology, I can help you and your team determine the most efficient strategy moving forward.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "brendano388@protonmail.com",
+    YOUR_FONE: "",
+    description: "",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+   
+    
+    linkedin: "https://www.linkedin.com/in/brendan-o%E2%80%99flaherty-32b88757/",
+    twitter: "https://twitter.com/_bcbread",
+    
+    
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
